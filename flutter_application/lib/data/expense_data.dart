1f321d9fcdd5d825b7f127464ed1e7c61a945136
Pumudu Nadun Rajakaruna <e19306@eng.pdn.ac.lk>
2024-03-23 10:09:22 +0530

    added expense data class, ExpenseItem class and date time helper

---
+++ flutter_application/lib/data/expense_data.dart
@@ -0,0 +1,60 @@
+import '/models/expense_item.dart';
+
+class ExpenseData {
+  //list of all expenses
+  List<ExpenseItem> overallExpenseList = [];
+
+  // get expense list
+  List<ExpenseItem> getAllExpenseList() {
+    return overallExpenseList;
+  }
+
+  // add expense
+  void addExpense(ExpenseItem newExpense) {
+    overallExpenseList.add(newExpense);
+  }
+
+  // delete expense
+  void deleteExpense(ExpenseItem expense) {
+    overallExpenseList.remove(expense);
+  }
+
+  //get weekday from a dateTime object
+  String getDayName(DateTime dateTime) {
+    switch (dateTime.weekday) {
+      case 1:
+        return 'Monday';
+      case 2:
+        return 'Tuesday';
+      case 3:
+        return 'Wednesday';
+      case 4:
+        return 'Thursday';
+      case 5:
+        return 'Friday';
+      case 6:
+        return 'Saturday';
+      case 7:
+        return 'Sunday';
+      default:
+        return 'Invalid';
+    }
+  }
+
+  //get the date for the start of the week
+  DateTime startOfWeekDate() {
+    DateTime? startOfWeek;
+
+    // get todays date
+    DateTime today = DateTime.now();
+
+    // go backward from today to find sunday
+    for (int i = 0; i < 7; i++) {
+      if (getDayName(today.subtract(Duration(days: i))) == 'Sunday') {
+        startOfWeek = today.subtract(Duration(days: i));
+        break;
+      }
+    }
+    return startOfWeek!;
+  }
+}
